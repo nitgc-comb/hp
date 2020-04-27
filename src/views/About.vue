d1014406580235c5f925695c608e2ff7185cc30f
snowsphere <prismistim@hotmail.com>
2020-04-28 01:13:04 +0900

    add: 各ページのテンプレートを追加

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div
+    section
+      SectionTitle(icon="check-circle" title="活動内容")
+      SectionContent(icon="laptop-code" imageLeft="members_2")
+        template(#title) C言語講座
+        template(#description)
+          | 4月から9月にかけて、新たに入った人を対象にC言語を用いたプログラミング講座を行っています。
+          | この講座で、プログラミングを基礎の基礎から分かりやすく、
+          | 丁寧に説明し、全員がある程度プログラミングスキルを習得できることを目指しています。
+      SectionContent(icon="gamepad" imageRight="typing")
+        template(#title) ゲーム制作
+        template(#description)
+          | C言語講座が終わると、得た知識を生かして各々でゲームの制作を行い始めます。ゲームのジャンルや制作環境などに制約はなく、それぞれ作りたいものを作ります。そして、
+          | 部内イベント「マスダヤモンの戦い」にて制作した作品を用いてコンテストを行います。さらに今度はその作品を来年度の高専祭にて展示し、
+          | 学内の学生や来場される一般の方々に試遊していただいています。
+      SectionContent(icon="users" imageLeft="procon_2019")
+        template(#title) 「高専プロコン」への参加
+        template(#description)
+          | 全国の高専規模で行われる大会はロボコンだけではありません。プログラミングの技術、発想を競う「全国高専プログラミングコンテスト」というものも毎年開催されています。
+          | プログラミングコンテスト(プロコン)では、プログラミングの技術を競う競技部門に加え、テーマについて新しいプロダクトを提案する課題部門、自由な発想で新しいプロダクトを提案する自由部門の3部門あり、
+          | 岐阜高専コンピュータ倶楽部は毎年各部門に参加しています。コンテストは、競技や発表以外にも、他高専の学生との交流もでき、参加した学生からは毎年とても充実していたと聞きます。
+    section
+      SectionTitle(icon="award" title="コンテスト参加・受賞歴")
+      h4.year-section-title 2019
+      AwardList(year="2019")
+      h4.year-section-title 2018
+      AwardList(year="2018")
+</template>
+
+<script>
+import SectionTitle from '../components/SectionTitle.vue'
+import SectionContent from '../components/SectionContent.vue'
+import AwardList from '../components/AwardList.vue'
+
+export default {
+  components: {
+    SectionTitle,
+    SectionContent,
+    AwardList
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  div
+    section.swing
+      h3.section-title
+        font-awesome-icon(icon="info-circle")
+        | どんな部活？
+      el-carousel(v-if="images" height="350px" :interval="5000" trigger="click")
+        el-carousel-item(v-for="item in images" :key="item")
+          img.section-content-img(:src="require(`@/assets/images/${item}.jpg`)")
+      ul.point
+        li.list-item
+          span.highlight プログラミング
+          | をして様々なことに取り組む部活です
+        li.list-item 活動は
+          span.highlight 月・水
+          | の放課後で週2回です
+          span.highlight (兼部も可能です)
+        li.list-item 基本、
+          span.highlight 情報処理センター2F第3演習室
+          | で活動しています
+        li.list-item プログラミングについて
+          span.highlight 何も分からなくても大丈夫！部員が丁寧に教えます
+        li.list-item
+          span.highlight プログラミング以外の創作活動
+          | も行っています
+      .control-zone
+        router-link.btn.router-btn(to="/about")
+          font-awesome-icon.icon-detail(icon="angle-double-right")
+          | 詳細を見る
+    section
+      h3.section-title
+        font-awesome-icon(icon="user-circle")
+        | 部員と顧問
+      .club-info
+        .club-info-item
+          .item-title 部員数
+          .item-value {{ clubInfo.members }}名
+        .club-info-item(v-for="item in clubInfo.advisor")
+          .item-title {{ item.type }}
+          .item-value {{ item.name }} ({{ item.department }})
+</template>
+
+<script>
+import info from '../assets/data.json'
+
+export default {
+  computed: {
+    images: () => ['members_2', 'plate', 'typing'],
+    clubInfo: () => info.info
+  }
+}
+</script>
