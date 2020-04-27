b9184affa94ba929c53c8a6b40280567c937eeef
snowsphere <prismistim@hotmail.com>
2020-04-28 01:14:27 +0900

    add: mainのjsファイルを追加

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { Carousel, CarouselItem } from 'element-ui'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './plugins/element.js'
+
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+
+library.add(faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+require('./styles/style.scss')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
