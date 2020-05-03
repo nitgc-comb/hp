518771a5721f2d68d7661bae0024e0f0d9829fd0
snowsphere <prismistim@hotmail.com>
2020-05-04 01:09:16 +0900

    add: 外部リンクアイコンの追加

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import router from './router'
 import { Carousel, CarouselItem } from 'element-ui'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './plugins/element.js'
 
 Vue.use(Carousel)
 Vue.use(CarouselItem)
 
-library.add(faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers)
+library.add(faInfoCircle, faUserCircle, faAngleDoubleRight, faCheckCircle, faAward, faLaptopCode, faGamepad, faUsers, faExternalLinkAlt)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 require('./styles/style.scss')
