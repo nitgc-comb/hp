d7393e87061a0f0f41c25b3b446ea33ad2da5792
snowsphere <prismistim@hotmail.com>
2020-05-04 01:13:36 +0900

    add: 画像の左右属性を追加

--- src/components/SectionContent.vue
+++ src/components/SectionContent.vue
@@ -1,21 +1,24 @@
 <template lang="pug">
-  .section-content
-    img.section-content-img(v-if="imageLeft" :src="require(`@/assets/images/${imageLeft}.jpg`)")
+  .section-content(:class="{ 'img-l': left, 'img-r': right}")
+    img.section-content-img(
+      v-if="image"
+      :src="require(`@/assets/images/${image}.jpg`)"
+    )
     .content-explain
       h4.sub-section-title
         font-awesome-icon(:icon="icon")
         slot(name="title")
       p.description
         slot(name="description")
-    img.section-content-img(v-if="imageRight" :src="require(`@/assets/images/${imageRight}.jpg`)")
 </template>
 
 <script>
 export default {
   props: [
     'icon',
-    'imageLeft',
-    'imageRight'
+    'image',
+    'left',
+    'right'
   ]
 }
 </script>
