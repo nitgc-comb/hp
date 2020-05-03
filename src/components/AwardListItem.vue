a898f2d0c8d06b5a3e2bc40f3263d5320ef1446e
snowsphere <prismistim@hotmail.com>
2020-05-04 01:13:01 +0900

    add: イベントのタイトルにアイコンを追加

--- src/components/AwardListItem.vue
+++ src/components/AwardListItem.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   .event-card
-    .event-title {{ title }}
+    .event-title
+      a(:href="url")
+        | {{ title }}
+        font-awesome-icon(icon="external-link-alt")
       .event-date {{ date }}
     .event-card-item(v-for="item in this.result")
       .item-category {{ item.category }}
