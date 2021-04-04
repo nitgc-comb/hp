bf549cd9f09a982da256c03850926ca50e2bac21
prismistim <prismistim@hotmail.com>
2021-04-05 05:19:51 +0900

    add: 体験講座のお知らせの追加

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .card
+    .card-title 令和3年度 体験講座について
+    .card-subtitle 2020-04-05
+    .card-text
+      | コンピュータ倶楽部では、今年度入部を検討している方に向けて体験講座を開く予定です。
+      | いまのところ詳細は未定です。
+      br
+      | 下のフォームでメールアドレスを登録すると、
+      span.highlight そのアドレスに講座の日程や内容をお知らせします
+      | ので、是非登録してください！
+    .card-actions
+      a.btn.router-btn(
+        target="blank"
+        href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZAAMm7jbpUREZTNUpIRDVINlBJS0w2NzZURDBTOVlGUC4u"
+      )
+        font-awesome-icon.icon-detail(icon="angle-double-right")
+        | 登録フォーム
+</template>
